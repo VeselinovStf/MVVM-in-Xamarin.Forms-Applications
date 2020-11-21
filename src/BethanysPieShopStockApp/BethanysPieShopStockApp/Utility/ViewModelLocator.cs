6a70abeffa96d407f65741347770b280e372a08d
s v <chofexx@gmail.com>
2020-11-21 13:51:39 +0200

    Adding View Model Locator

---
+++ src/BethanysPieShopStockApp/BethanysPieShopStockApp/Utility/ViewModelLocator.cs
@@ -0,0 +1,11 @@
+﻿using BethanysPieShopStockApp.ViewModels;
+
+namespace BethanysPieShopStockApp.Utility
+{
+    public static class ViewModelLocator
+    {
+        public static PieDetailsViewModel PieDetailViewModel { get; set; } = new PieDetailsViewModel();
+
+        public static PieOverviewViewModel PieOverviewViewModel { get; set; } = new PieOverviewViewModel();
+    }
+}
--- src/BethanysPieShopStockApp/BethanysPieShopStockApp/Views/PieDetailView.xaml.cs
+++ src/BethanysPieShopStockApp/BethanysPieShopStockApp/Views/PieDetailView.xaml.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using BethanysPieShopStockApp.Utility;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -15,6 +11,8 @@ namespace BethanysPieShopStockApp.Views
         public PieDetailView()
         {
             InitializeComponent();
+
+            BindingContext = ViewModelLocator.PieDetailViewModel;
         }
     }
 }
