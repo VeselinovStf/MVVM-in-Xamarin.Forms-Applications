9a52c48b43b529240437ca4499a8ded724c167fd
s v <chofexx@gmail.com>
2020-11-21 11:25:08 +0200

    Adding Model and Mock -> Db and Repository

---
+++ src/BethanysPieShopStockApp/BethanysPieShopStockApp/Models/Pie.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace BethanysPieShopStockApp.Models
+{
+    public class Pie
+    {
+        private Guid _id;
+        private string _pieName;
+        private string _description;
+        private double _price;
+        private string _imageUrl;
+        private bool _inStock;
+
+        public Guid Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+     
+        public string PieName
+        {
+            get { return _pieName; }
+            set { _pieName = value; }
+        }
+       
+        public string Description
+        {
+            get { return _description; }
+            set { _description = value; }
+        }
+       
+        public double Price
+        {
+            get { return _price; }
+            set { _price = value; }
+        }
+       
+        public string ImageUrl
+        {
+            get { return _imageUrl; }
+            set { _imageUrl = value; }
+        }
+       
+        public bool InStock
+        {
+            get { return _inStock; }
+            set { _inStock = value; }
+        }
+
+
+    }
+}
---
+++ src/BethanysPieShopStockApp/BethanysPieShopStockApp/Repositories/PieRepository.cs
@@ -0,0 +1,28 @@
+﻿using BethanysPieShopStockApp.Models;
+using BethanysPieShopStockApp.Services;
+using System;
+using System.Linq;
+
+namespace BethanysPieShopStockApp.Repositories
+{
+    public class PieRepository
+    {
+
+        public void AddPie(Pie pie)
+        {
+            pie.Id = Guid.NewGuid();
+            pie.ImageUrl = "https://gillcleerenpluralsight.blob.core.windows.net/files/cherrypiesmall.jpg";
+
+            MockDbService.Pies.Add(pie);
+        }
+
+        public void UpdatePie(Pie pie)
+        {
+            var oldPie = MockDbService.Pies.Where(p => p.Id == pie.Id).FirstOrDefault();
+            oldPie.PieName = pie.PieName;
+            oldPie.Price = pie.Price;
+            oldPie.Description = pie.Description;
+            oldPie.InStock = pie.InStock;
+        }
+    }
+}
